9ef32da13aeccababae98fcd44e6ba8640e87a61
Uliana <159609886+Programmist2003@users.noreply.github.com>
2024-03-26 19:33:30 +0300

    Add files via upload

---
+++ ConverterAge/Program.cs
@@ -0,0 +1,18 @@
+﻿            // Текущая дата
+            DateTime Today = DateTime.Today;
+ 
+            // День рождения
+            DateTime Birthday = new DateTime(2003, 07, 10);
+ 
+            // Возраст в годах
+            int YearCount = 19;
+ 
+            if (Today.Month <= Birthday.Month && Today.Day < Birthday.Day)
+            {
+                YearCount = Convert.ToInt32(Today.Year - Birthday.Year) - 1;
+            }
+            else
+            {
+                YearCount = Convert.ToInt32(Today.Year - Birthday.Year);
+            }
+            Console.WriteLine(YearCount);
